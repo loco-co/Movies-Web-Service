77c7a86aa2673821d5f69ed5a3457095490b42db
loco-co <loco2925@gmail.com>
2022-03-29 00:06:41 +0900

    메인페이지 섹션 추가, IMDB

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,17 @@
 import propTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styles from "./Movie.module.css"
+import imdb from "../images/IMDB.png"
 
-function Movie({ id, title, thumb_img, year, summary, genres }) {
+function Movie({ id, title, thumb_img, year, summary, genres, rating }) {
   return (
     <div className={styles.movie}>
       <Link to={`/movie/${id}`}><img className={styles.movie__image} src={thumb_img} alt={title} /></Link>
       <h1 className={styles.movie__title}><Link to={`/movie/${id}`}>{title}</Link></h1>
+      <div className={styles.movie__info}>
       <h3 className={styles.movie__year}>{year}</h3>
+      <img className={styles.movie__logo} src={imdb} /><h3 className={styles.movie__rating}>{rating}</h3>
+      </div>
       <ul className={styles.movie__genres}>
         {genres.map((g) => (
           <li key={g}>{g}</li>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,33 +4,81 @@ import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState("true");
-  const [movies, setMovies] = useState([]);
+  const [recentMovies, setRecentMovies] = useState([]);
+  const [highMovies, setHighMovies] = useState([]);
+  const [ratedMovies, setRatedMovies] = useState([]);
   const getMovies = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-    );
-    const json = await response.json();
+    const [response1, response2, response3] = await Promise.all([
+      fetch(
+        `https://yts.mx/api/v2/list_movies.json?sort_by=year&with_rt_ratings=true&limit=6`
+      ),
+      fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8&with_rt_ratings=true&limit=6`
+      ),
+      fetch(
+        `https://yts.mx/api/v2/list_movies.json?quality=2160p&with_rt_ratings=true&limit=6`
+      ),
+    ]);
+    const json1 = await response1.json();
+    const json2 = await response2.json();
+    const json3 = await response3.json();
     setLoading(false);
-    setMovies(json.data.movies);
+    setRecentMovies(json1.data.movies);
+    setRatedMovies(json2.data.movies);
+    setHighMovies(json3.data.movies);
   };
   useEffect(() => getMovies(), []);
   return (
-    <div className={styles.container}>
+    <div>
       {loading ? (
         <h1 className={styles.loader}>Loading...</h1>
       ) : (
-        <div className={styles.movies}>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              thumb_img={movie.medium_cover_image}
-              title={movie.title}
-              year={movie.year}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
+        <div className={styles.container}>
+          <h1 className={styles.title}>Most Recent Movies</h1>
+          <div className={styles.movies}>
+            {recentMovies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                thumb_img={movie.medium_cover_image}
+                title={movie.title}
+                year={movie.year}
+                summary={movie.summary}
+                genres={movie.genres}
+                rating={movie.rating}
+              />
+            ))}
+          </div>
+          <h1 className={styles.title}>2160p Quality Movies</h1>
+          <div className={styles.movies}>
+            {highMovies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                thumb_img={movie.medium_cover_image}
+                title={movie.title}
+                year={movie.year}
+                summary={movie.summary}
+                genres={movie.genres}
+                rating={movie.rating}
+              />
+            ))}
+          </div>
+          <h1 className={styles.title}>Top Rated Movies</h1>
+          <div className={styles.movies}>
+            {ratedMovies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                thumb_img={movie.medium_cover_image}
+                title={movie.title}
+                year={movie.year}
+                summary={movie.summary}
+                genres={movie.genres}
+                rating={movie.rating}
+              />
+            ))}
+          </div>
         </div>
       )}
     </div>
